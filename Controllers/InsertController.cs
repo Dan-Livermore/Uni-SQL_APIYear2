013ddab4c6e169435af68bf22f48f0559351c342
Dan-Livermore <daniel.livermore@students.plymouth.ac.uk>
2023-01-10 11:14:08 +0000

    Added new controllers

---
+++ Controllers/InsertController.cs
@@ -0,0 +1,20 @@
+﻿using CW2_TrailService.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+
+namespace CW2_TrailService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InsertController : ControllerBase
+    {
+        private readonly Comp2001DlivermoreContext _context;
+
+        public InsertController(Comp2001DlivermoreContext context)
+        {
+            _context = context;
+        }
+
+    }
+}
---
+++ Controllers/SelectController.cs
@@ -0,0 +1,22 @@
+﻿using CW2_TrailService.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CW2_TrailService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SelectController : ControllerBase
+    {
+            private readonly Comp2001DlivermoreContext _context;
+
+            public SelectController(Comp2001DlivermoreContext context)
+            {
+                _context = context;
+            }
+
+            [HttpGet("SP")]
+            public async Task<ActionResult<>>
+ 
+    }
+}
