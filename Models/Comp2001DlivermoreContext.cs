c22705206aab33f5458df8e821facb8cad30258e
Dan Livermore <91542245+Dan-Livermore@users.noreply.github.com>
2024-05-06 15:41:08 +0100

    Update Comp2001DlivermoreContext.cs

--- Models/Comp2001DlivermoreContext.cs
+++ Models/Comp2001DlivermoreContext.cs
@@ -30,7 +30,7 @@ public partial class Comp2001DlivermoreContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=socem1.uopnet.plymouth.ac.uk;Database=COMP2001_DLivermore; User Id=DLivermore; Password=KnwS907+; TrustServerCertificate=True");
+        => optionsBuilder.UseSqlServer("Server=socem1.uopnet.plymouth.ac.uk;Database=COMP2001_DLivermore; User Id=DLivermore; Password=<Password>; TrustServerCertificate=True");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
